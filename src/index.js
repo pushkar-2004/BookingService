84ccb9aab1e391d4c61c29c7558c70e9f235ff0c
Pushkar Kumar <qwertypushkar123@gmail.com>
2026-01-09 15:46:10 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+function setupAndStartServer() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('./api',apiRoutes);
+  app.listen(PORT, () => {
+    console.log(`Server started on port number ${PORT}`);
+    if(process.env.DB_SYNC){
+      db.sequelize.sync({alter:true});
+    }
+  });
+}
+
+setupAndStartServer();
