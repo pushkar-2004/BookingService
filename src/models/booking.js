1920f3683074946353eb0a246153f0c5712324ae
Pushkar Kumar <qwertypushkar123@gmail.com>
2026-01-10 12:29:57 +0530

    added new column to booking tables

--- src/models/booking.js
+++ src/models/booking.js
@@ -21,6 +21,16 @@ module.exports = (sequelize, DataTypes) => {
         values:['UnderProcess','Booked','Cancelled'],
         defaultValue: "UnderProcess",
       },
+      noOfSeats: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue: 1
+      },
+      totalCost: {
+        type: DataTypes.INTEGER,
+        allowNull : false,
+        defaultValue : 0
+      }
     },
     {
       sequelize,
